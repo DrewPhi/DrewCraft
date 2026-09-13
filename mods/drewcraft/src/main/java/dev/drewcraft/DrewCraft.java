5f62f9b9923d0022f9c6df51c25fc4c7f86ca18a
Drew Steindl <Drewsteindl@icloud.com>
2026-09-12 20:38:13 -0400

    Wire Source Core registration and production scheduler

--- mods/drewcraft/src/main/java/dev/drewcraft/DrewCraft.java
+++ mods/drewcraft/src/main/java/dev/drewcraft/DrewCraft.java
@@ -3,10 +3,12 @@ package dev.drewcraft;
 import com.mojang.logging.LogUtils;
 import dev.drewcraft.command.DrewCraftCommands;
 import dev.drewcraft.config.DrewCraftConfig;
+import dev.drewcraft.content.DrewCraftBlocks;
 import dev.drewcraft.net.DrewCraftProtocol;
 import dev.drewcraft.persistence.DrewCraftSavedData;
 import dev.drewcraft.strategic.encounter.StrategicMaterializationRuntime;
 import dev.drewcraft.strategic.simulation.StrategicScheduler;
+import dev.drewcraft.strategic.source.SourceProductionScheduler;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
@@ -23,12 +25,14 @@ public final class DrewCraft {
 
     public DrewCraft(IEventBus modEventBus, ModContainer modContainer) {
         modVersion = modContainer.getModInfo().getVersion().toString();
+        DrewCraftBlocks.register(modEventBus);
         modContainer.registerConfig(ModConfig.Type.SERVER, DrewCraftConfig.SPEC);
         NeoForge.EVENT_BUS.addListener(DrewCraftCommands::register);
         NeoForge.EVENT_BUS.addListener(StrategicScheduler::onServerTick);
         NeoForge.EVENT_BUS.addListener(StrategicMaterializationRuntime::onServerTick);
         NeoForge.EVENT_BUS.addListener(StrategicMaterializationRuntime::onLivingDeath);
         NeoForge.EVENT_BUS.addListener(StrategicMaterializationRuntime::onEntityJoinLevel);
+        NeoForge.EVENT_BUS.addListener(SourceProductionScheduler::onServerTick);
 
         LOGGER.info(
                 "DrewCraft integration platform {} loaded (protocol {}, persistence schema {})",
