493d1ae446ca357531391548e13dba411ae89605
Drew Steindl <Drewsteindl@icloud.com>
2026-09-12 21:28:53 -0400

    Register BP7 herd commands

--- mods/drewcraft/src/main/java/dev/drewcraft/command/DrewCraftCommands.java
+++ mods/drewcraft/src/main/java/dev/drewcraft/command/DrewCraftCommands.java
@@ -39,6 +39,7 @@ public final class DrewCraftCommands {
                                         .executes(context -> samplePower(context.getSource()))))
                         .then(StrategicCommands.node())
                         .then(SourceCommands.node())
+                        .then(HerdCommands.node())
         );
     }
 
