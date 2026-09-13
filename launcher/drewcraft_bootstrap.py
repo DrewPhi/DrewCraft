6c2b8ec77ac160a63be11aab6dab8d0977b7e0c6
DrewPhi <drewsteindl@icloud.com>
2026-09-13 12:08:42 -0400

    Show launcher download progress and resume installs

--- launcher/drewcraft_bootstrap.py
+++ launcher/drewcraft_bootstrap.py
@@ -22,7 +22,7 @@ import time
 import urllib.request
 import zipfile
 
-APP_VERSION = "0.1.0"
+APP_VERSION = "0.1.1"
 PRESERVED_USER_PATHS = ("screenshots", "resourcepacks", "shaderpacks", "saves", "options.txt")
 
 
